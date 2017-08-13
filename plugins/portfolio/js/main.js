4415af1d978b2bef0f380e8ec17829588ff8f455
AliSinaSaemi <a.s.a.j.2016@gmail.com>
2017-08-13 10:06:57 +0430

    2 pages with their animations are compeleted and those will help us out to other pages.

--- plugins/portfolio/js/main.js
+++ plugins/portfolio/js/main.js
@@ -67,6 +67,13 @@ var articleTitles = {
   scale: 0.8
 }
 sr.reveal('.article-title', articleTitles);
+var underlineTitle = {
+  delay: 500,
+  scale: 0.8,
+  origin: 'bottom',
+  distance: '30px',
+}
+sr.reveal('.sub-underline-title', underlineTitle);
 
 var footerContentRight = {
   delay: 200,
@@ -101,6 +108,41 @@ var footerContentCopyright = {
 sr.reveal('.footer-content-4', footerContentCopyright)
 
 // main page
+var bannerContent = {
+  delay: 300,
+  duration: 800,
+  origin: 'bottom',
+  distance: '20px',
+}
+sr.reveal('.banner-content', bannerContent);
+var rotatingSlider = {
+  delay: 1000,
+  duration: 1000,
+  origin: 'bottom',
+  distance: '20px',
+}
+sr.reveal('.rotating-slider-list', rotatingSlider);
+var rotatingSliderRightArrow = {
+  delay: 700,
+  duration: 800,
+    origin: 'left',
+  distance: '20px',
+}
+sr.reveal('.rm_next', rotatingSliderRightArrow);
+var rotatingSliderLeftArrow = {
+  delay: 700,
+  duration: 800,
+    origin: 'right',
+  distance: '20px',
+}
+sr.reveal('.rm_prev', rotatingSliderLeftArrow);
+var rotatingSliderText = {
+  delay: 1500,
+  duration: 800,
+  origin: 'right',
+  distance: '30px',
+}
+sr.reveal('.rotating-slider-text', rotatingSliderText);
 
 // about page
 var aboutText = {
@@ -113,42 +155,30 @@ sr.reveal('.section-1-about', aboutText);
 sr.reveal('.section-2-about', aboutText);
 sr.reveal('.section-3-about', aboutText);
 
-/*
-      var hero = {
-        origin   : "top",
-        distance : "24px",
-        duration : 1500,
-        scale    : 1.05,
-      }
-
-      var intro = {
-        origin   : "bottom",
-        distance : "64px",
-        duration : 900,
-        delay    : 1500,
-        scale    : 1,
-      }
-
-      var github = {
-        origin   : "top",
-        distance : "32px",
-        duration : 600,
-        delay    : 1800,
-        scale    : 0,
-      }
-
-      var block = {
-        reset: true,
-        viewOffset: { top: 64 }
-      }
-
-      sr.reveal(".demo .block", block)
-      sr.reveal(".hero", hero)
-      sr.reveal(".intro", intro)
-      sr.reveal(".hero .github", github)
-      sr.reveal(".seq-1", block, 200)
-
-      function addCommasToNum( num ){
-        return num.toString().replace( /\B(?=(\d{3})+(?!\d))/g, "," )
-      }    
-*/
+// gallary page
+var gallaryGateRight = {
+  delay: 900,
+  duration: 500,
+  origin: 'right',
+  distance: '30px',
+}
+var gallaryGateCenter = {
+  delay: 700,
+  duration: 500,
+}
+var gallaryGateLeft = {
+  delay: 900,
+  duration: 500,
+  origin: 'left',
+  distance: '30px',
+}
+sr.reveal('.gallary-right', gallaryGateRight);
+sr.reveal('.gallary-center', gallaryGateCenter);
+sr.reveal('.gallary-left', gallaryGateLeft);
+var gallaryContinue = {
+  delay: 1000,
+  duration: 500,
+  origin: 'top',
+  distance: '30px',
+}
+sr.reveal('.gallary-continue', gallaryContinue);
